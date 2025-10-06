c6741b0c30a42eabf070909ba7a37a62178b1675
BrianJBlandino <bblandi@iu.edu>
2025-10-06 13:07:00 -0400

    rehgswrfhysrfdhsd

--- apputil.py
+++ apputil.py
@@ -1,1 +1,12 @@
-# your code here ...
+class Genius:
+    """
+    A class to interact with the Genius API.
+    """
+    def __init__(self, access_token):
+        """
+        Initialize the Genius object and save the access token as an attribute.
+        
+        Parameters:
+        access_token (str): The API access token.
+        """
+        self.access_token = access_token
